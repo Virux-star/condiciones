6f745d1524e04f118ff167131e82c193a69408ac
virux <jantonioascencio10@gmail10.com>
2026-02-08 19:44:47 -0300

    agregue parte 2

--- script.js
+++ script.js
@@ -7,3 +7,15 @@ imagen.addEventListener("click", () => {
     }
     
 });
+
+ document.getElementById("btnStickers").addEventListener("click", () => {
+      const s1 = Number(document.getElementById("sticker1").value);
+      const s2 = Number(document.getElementById("sticker2").value);
+      const s3 = Number(document.getElementById("sticker3").value);
+      const total = s1 + s2 + s3;
+
+      const resultado = document.getElementById("resultadoStickers");
+      resultado.textContent = total <= 10
+        ? `Llevas ${total} stickers`
+        : "Llevas demasiados stickers";
+    });
